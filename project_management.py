557e105510e10ccb8dbd8a05c0a9806851860907
frank liang <348017715@qq.com>
2015-01-21 09:51:45 +0800

    renamed files

--- project_management.py
+++
@@ -1,8 +0,0 @@
-from openerp.osv import osv, fields
-
-class ProjectManagement(osv.Model):
-	_name = "hrestimate.projectmanagement"
-
-	_columns = {
-		'test' : fields.char(string="Test", size=256, required=True),
-	}
